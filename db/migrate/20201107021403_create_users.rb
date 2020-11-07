15fb045911657af76bb7d2d4e4532a554c27b7c2
epittab <ep7m@me.com>
2020-11-06 21:31:14 -0500

    initial db setup

---
+++ db/migrate/20201107021403_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :gender
+      t.date :birthdate
+      t.integer :height
+      t.integer :weight
+      t.integer :zip_code
+
+      t.timestamps
+    end
+  end
+end
