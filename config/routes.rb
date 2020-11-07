33a5004c3c92eb7a5059a4864893cc255d4e5658
epittab <ep7m@me.com>
2020-11-06 21:46:01 -0500

    added most general controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  
+  namespace :api do
+    namespace :v1 do
+
+      # event routing
+      get '/events', to: 'event#test'
+
+      # avatar routing
+
+
+      # body location routing
+
+
+      # user routing
+
+
+      # disease routing
+
+
+      # auth routing
+      post '/login', to: 'auth#login'
+      post '/register', to: 'auth#register'
+
+    end
+  end
 end
