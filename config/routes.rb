500c19d175c278bdb6c57ef98d36a8297168318a
gabejoseph <gabriel.joseph4@gmail.com>
2020-11-11 13:24:01 -0500

    trying without namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  resources :users
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+
   namespace :api do
     namespace :v1 do
 
@@ -15,9 +19,6 @@ Rails.application.routes.draw do
 
       # user routing
 
-      resources :users
-      resources :sessions, only: [:create]
-      resources :registrations, only: [:create]
 
       # disease routing
 
