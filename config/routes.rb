f11f2471c1b2293a964478410c2f4489c85d4188
gabejoseph <gabriel.joseph4@gmail.com>
2020-11-12 13:21:15 -0500

    incorporated python api and route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
+  post '/logresult', to: 'api#logresult'
 
   namespace :api do
     namespace :v1 do
